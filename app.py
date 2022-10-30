ab5070296a0ef934bb0ae3ea7ab94f7919d2a1c2
UnMeloow <unkmelow@gmail.com>
2022-10-30 19:16:41 +0300

    init

---
+++ app.py
@@ -0,0 +1,58 @@
+import os
+import glob
+from keras.models import load_model
+from flask import Flask, render_template, request, send_from_directory
+from process_image import classify_image
+
+app = Flask(__name__)
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+UPLOAD_FOLDER = "uploads/"
+STATIC_FOLDER = "static"
+
+# Load model
+model = load_model(STATIC_FOLDER + "/eye_closed_detect_model_2.0.0.h5")
+
+IMAGE_SIZE = 24
+
+
+# home page
+@app.route("/", methods=['GET'])
+def home():
+    filelist = glob.glob("uploads/*.*")
+    for filePath in filelist:
+        try:
+            os.remove(filePath)
+        except:
+            print("Error while deleting file")
+    return render_template("home.html")
+
+
+@app.route("/classify", methods=["POST", "GET"])
+def upload_file():
+
+    if request.method == "GET":
+        return render_template("home.html")
+
+    else:
+        file = request.files["image"]
+        upload_image_path = os.path.join(UPLOAD_FOLDER, file.filename.replace("\\\\", ""))
+        print(file.filename)
+        file.save(upload_image_path)
+
+        l_res, r_res = classify_image(upload_image_path, model, IMAGE_SIZE)
+
+    return render_template(
+        "classify.html", image_file_name=file.filename, l_res=l_res, r_res=r_res
+    )
+
+
+@app.route("/classify/<filename>")
+def send_file(filename):
+    return send_from_directory(UPLOAD_FOLDER, filename)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
+    app.run()
+    
---
+++ process_image.py
@@ -0,0 +1,146 @@
+import cv2
+import dlib
+import numpy as np
+from imutils import face_utils
+from PIL import Image
+
+
+class Colors:
+    BLACK = (0, 0, 0)
+    RED = (0, 0, 255)
+    GREEN = (0, 255, 0)
+    BLUE = (255, 0, 0)
+    YELLOW = (0, 255, 255)
+    VIOLET = (255, 0, 255)
+    CYAN = (255, 255, 0)
+    WHITE = (255, 255, 255)
+    LIGHT_GREY = (128, 128, 128)
+    DARK_GREY = (60, 60, 60)
+
+
+predictor = dlib.shape_predictor('./static/shape_predictor_68_face_landmarks.dat')
+face_cascade = cv2.CascadeClassifier('./static/haarcascade_frontalface_alt.xml')
+
+
+# detect the face rectangle
+def detect(img, cascade=face_cascade, minimumFeatureSize=(20, 20)):
+    if cascade.empty():
+        raise (Exception("There was a problem loading your Haar Cascade xml file."))
+    rects = cascade.detectMultiScale(img, scaleFactor=1.3, minNeighbors=1, minSize=minimumFeatureSize)
+
+    # if it doesn't return rectangle return array
+    # with zero lenght
+    if len(rects) == 0:
+        return []
+
+    #  convert last coord from (width,height) to (maxX, maxY)
+    rects[:, 2:] += rects[:, :2]
+
+    return rects
+
+
+def cnnPreprocess(img):
+    img = img.astype('float32')
+    img /= 255
+    img = np.expand_dims(img, axis=2)
+    img = np.expand_dims(img, axis=0)
+    return img
+
+
+def cropEyes(frame, im_size):
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # detect the face at grayscale image
+    te = detect(gray, minimumFeatureSize=(80, 80))
+
+    # if the face detector doesn't detect face
+    # return None, else if detects more than one faces
+    # keep the bigger and if it is only one keep one dim
+    if len(te) == 0:
+        return None
+    elif len(te) > 1:
+        face = te[0]
+    elif len(te) == 1:
+        [face] = te
+
+    # keep the face region from the whole frame
+    face_rect = dlib.rectangle(left=int(face[0]), top=int(face[1]),
+                               right=int(face[2]), bottom=int(face[3]))
+
+    # determine the facial landmarks for the face region
+    shape = predictor(gray, face_rect)
+    shape = face_utils.shape_to_np(shape)
+
+    #  grab the indexes of the facial landmarks for the left and
+    #  right eye, respectively
+    (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+    (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+
+    # extract the left and right eye coordinates
+    leftEye = shape[lStart:lEnd]
+    rightEye = shape[rStart:rEnd]
+
+    # keep the upper and the lower limit of the eye
+    # and compute the height
+    l_uppery = min(leftEye[1:3, 1])
+    l_lowy = max(leftEye[4:, 1])
+    l_dify = abs(l_uppery - l_lowy)
+
+    # compute the width of the eye
+    lw = (leftEye[3][0] - leftEye[0][0])
+
+    minxl = (leftEye[0][0] - ((im_size - lw) / 2))
+    maxxl = (leftEye[3][0] + ((im_size - lw) / 2))
+    minyl = (l_uppery - ((im_size - l_dify) / 2))
+    maxyl = (l_lowy + ((im_size - l_dify) / 2))
+
+    # crop the eye rectangle from the frame
+    left_eye_rect = np.rint([minxl, minyl, maxxl, maxyl])
+    left_eye_rect = left_eye_rect.astype(int)
+    left_eye_image = gray[(left_eye_rect[1]):left_eye_rect[3], (left_eye_rect[0]):left_eye_rect[2]]
+
+    # same as left eye at right eye
+    r_uppery = min(rightEye[1:3, 1])
+    r_lowy = max(rightEye[4:, 1])
+    r_dify = abs(r_uppery - r_lowy)
+    rw = (rightEye[3][0] - rightEye[0][0])
+    minxr = (rightEye[0][0] - ((im_size - rw) / 2))
+    maxxr = (rightEye[3][0] + ((im_size - rw) / 2))
+    minyr = (r_uppery - ((im_size - r_dify) / 2))
+    maxyr = (r_lowy + ((im_size - r_dify) / 2))
+    right_eye_rect = np.rint([minxr, minyr, maxxr, maxyr])
+    right_eye_rect = right_eye_rect.astype(int)
+    right_eye_image = gray[right_eye_rect[1]:right_eye_rect[3], right_eye_rect[0]:right_eye_rect[2]]
+
+    # if it doesn't detect left or right eye return None
+
+    if 0 in left_eye_image.shape or 0 in right_eye_image.shape:
+        return None
+    # resize for the conv net
+    left_eye_image = cv2.resize(left_eye_image, (im_size, im_size))
+    right_eye_image = cv2.resize(right_eye_image, (im_size, im_size))
+    left_eye_image = cv2.flip(right_eye_image, 1)
+    # return left and right eye
+    return left_eye_image, right_eye_image
+
+
+def classify_image(image_path, eye_model, im_size):
+    image = cv2.imread(image_path)
+    left_eye, right_eye = cropEyes(image, im_size)
+
+    left_eye = cnnPreprocess(left_eye)
+    right_eye = cnnPreprocess(right_eye)
+    left_eye_pred = eye_model.predict(left_eye)
+    right_eye_pred = eye_model.predict(right_eye)
+    left_eye_res = "Ваш левый глаз: "
+    right_eye_res = "Ваш правый глаз: "
+    if left_eye_pred <= 0.1:
+        left_eye_res += "закрыт."
+    else:
+        left_eye_res += "открыт."
+    if right_eye_pred <= 0.1:
+        right_eye_res += "закрыт."
+    else:
+        right_eye_res += "открыт."
+
+    return left_eye_res, right_eye_res
